6c3ec9e5eb9f54f3dcb913e1fcdc867ce9ff2bca
Nayan Pareek <nayan.pareek@gmail93.com>
2021-04-07 20:02:45 +0530

    first commit

---
+++ socialmedia.php
@@ -0,0 +1,9 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<!-- Add font awesome icons -->
+<font size = "10">
+<a href="https://www.facebook.com/padharo.rajasthan.3/" class="fa fa-facebook" ></a>
+<br><br><a href="https://twitter.com/?lang=en" class="fa fa-twitter"></a>
+<br><br><a href="https://www.instagram.com/_padharorajasthan/" class="fa fa-instagram"></a>
+<br><br><a href="https://www.youtube.com" class="fa fa-youtube"></a>
+<br><br><a href="#" class="fa fa-linkedin"></a>
