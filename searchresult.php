ce8be03ba2a2d24b4c068b85d2b20beb87ff02fa
Gauravdarkslayer <www.gaurav10bhojwani@gmail.com>
2021-05-03 21:04:48 +0530

    added new

--- searchresult.php
+++ searchresult.php
@@ -7,6 +7,16 @@ $result = mysqli_query($conn , $q) or die (mysqli_error($conn));
 
     while ($row = mysqli_fetch_array($result)) {
         echo "<br>";
-        printf("city: %s\n place:%s \n :%s: \n", $row['city'],$row['place'], $row['description']);
+        // printf("city: %s\n place:%s \n :%s: \n", $row['city'],$row['place'], $row['image']);
+        $image = $row['image'];
+        $description = $row['description'];
     }
 ?>
+
+
+<div class="card" style="width: 28rem; display:flow-root; margin:auto;">
+  <img class="card-img-top" height="100" Width="100" src=<?php echo $image; ?> alt="Card image cap">
+  <div class="card-body">
+    <p class="card-text"><?php echo$description ?></p>
+  </div>
+</div>
