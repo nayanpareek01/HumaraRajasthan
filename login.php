f2fd85ce70437b80d35f122f1e1500e253e538e9
Nayan Pareek <nayan.pareek@gmail93.com>
2021-05-06 19:58:48 +0530

    mesg from nayan

--- login.php
+++ login.php
@@ -9,7 +9,7 @@ if (isset($_POST['login'])) {
   $password = $_POST['pass'];
   mysqli_real_escape_string($conn, $username);
   mysqli_real_escape_string($conn, $password);
-$query = "SELECT * FROM users WHERE username = '$username'";
+$query = "SELECT * FROM user WHERE username = '$username'";
 $result = mysqli_query($conn , $query) or die (mysqli_error($conn));
 if (mysqli_num_rows($result) > 0) {
   while ($row = mysqli_fetch_array($result)) {
@@ -18,33 +18,25 @@ if (mysqli_num_rows($result) > 0) {
     $pass = $row['password'];
     $name = $row['name'];
     $email = $row['email'];
-    $role= $row['role'];
-    $course = $row['course'];
     if (password_verify($password, $pass )) {
       $_SESSION['id'] = $id;
       $_SESSION['username'] = $username;
       $_SESSION['name'] = $name;
       $_SESSION['email']  = $email;
-      $_SESSION['role'] = $role;
-      $_SESSION['course'] = $course;
       header('location: dashboard/');
     }
     else {
       echo "<script>alert('invalid username/password');
       window.location.href= 'login.php';</script>";
-
     }
   }
 }
 else {
       echo "<script>alert('invalid username/password');
       window.location.href= 'login.php';</script>";
-
     }
 }
 ?>
-
-
   <div class="login-card">
     <h1>Log-in</h1><br>
   <form method="POST">
@@ -60,7 +52,6 @@ else {
 
   <script src='css/jquery.min.js'></script>
 <script src='css/jquery-ui.min.js'></script>
-
   
 </body>
 </html>
--- searchresult.php
+++ searchresult.php
@@ -7,6 +7,23 @@ $result = mysqli_query($conn , $q) or die (mysqli_error($conn));
 
     while ($row = mysqli_fetch_array($result)) {
         echo "<br>";
-        printf("city: %s\n place:%s \n :%s: \n", $row['city'],$row['place'], $row['description']);
-    }
+        // printf("city: %s\n place:%s \n :%s: \n", $row['city'],$row['place'], $row['image']);
+               
+        $image = $row['image'];
+        $description = $row['description'];
+        $city=$row['city'];
+        $place=$row['place'];
+  
+?>
+<div class="card" style="background-color:pink; width: 28rem; display:flow-root; margin:auto;">
+<p class="card-text"><b>City Name: </b><?php echo$city ?></p>
+    <p class="card-text"><b>Place: </b><?php echo$place ?></p>
+     <img class="card-img-top" style="margin-left:12.5%;" height="200" Width="200" src=<?php echo $image; ?> alt="Card image cap">
+  <div class="card-body">
+    <p class="card-text"><b>Description: </b><br><?php echo$description ?></p>
+   
+  </div>
+</div>
+<?php
+}
 ?>
